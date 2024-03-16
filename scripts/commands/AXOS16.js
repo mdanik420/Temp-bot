2443b72ae34fd02991d7b1d10fa85f962dc8d120
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-16 23:18:37 +0800

    Add files via upload

---
+++ scripts/commands/AXOS16.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "🙋‍♂️",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "VanHung - Fixed by LTD", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "🙋‍♂️",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("amogus")==0 || event.body.indexOf("ওই")==0 || event.body.indexOf("oi")==0 || event.body.indexOf("kui gela")==0) {
+		var msg = {
+				body: "~ ডাকো কেনো গো, আমি আছি তো এখানে..!✋🥀",
+				attachment: fs.createReadStream(__dirname + `/noprefix/dakoknogo.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🙋‍♂️", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+      }
