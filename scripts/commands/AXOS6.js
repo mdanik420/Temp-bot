e21416cc45f2fbee5e4945d5e7ed7f4925aafacd
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-16 22:05:30 +0800

    Create AXOS6.js

---
+++ scripts/commands/AXOS6.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "😋",
+  prefix: true,
+    version: "1.0.1",
+	permssion: 0,
+	credits: "VanHung - Fixed by LTD", 
+	description: "hihihihi",
+	category: "no prefix",
+	usages: "😋",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("amogus")==0 || event.body.indexOf("Amogus")==0 || event.body.indexOf("🤤")==0 || event.body.indexOf("😋")==0) {
+		var msg = {
+				body: "~ বাবু খাইছো.. 🥀",
+				attachment: fs.createReadStream(__dirname + `/noprefix/babukaiso.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😽", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
