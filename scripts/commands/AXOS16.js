4871550c327166df949b72373535f89c303d4e11
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-16 23:19:25 +0800

    Update AXOS16.js

--- scripts/commands/AXOS16.js
+++ scripts/commands/AXOS16.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 module.exports.config = {
 	name: "🙋‍♂️",
+	prefix: true,
     version: "1.0.1",
-	hasPermssion: 0,
+	permssion: 0,
 	credits: "VanHung - Fixed by LTD", 
 	description: "hihihihi",
-	commandCategory: "no prefix",
+	category: "no prefix",
 	usages: "🙋‍♂️",
     cooldowns: 5, 
 };
