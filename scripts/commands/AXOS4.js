040d92f4d3dc22200f469f54d2ab305bbc3e00b7
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-16 21:56:36 +0800

    Create AXOS4.js

---
+++ scripts/commands/AXOS4.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "😎",
+  prefix: true,
+    version: "1.0.1",
+	permssion: 0,
+	credits: "VanHung - Fixed by LTD", 
+	description: "hihihihi",
+	category: "no prefix",
+	usages: "😎",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("amogus")==0 || event.body.indexOf("🥱")==0 || event.body.indexOf("🥴")==0 || event.body.indexOf("😎")==0) {
+		var msg = {
+				body: "~  তুমি Attitude দেখাছো, তাতে আমার বাল ছেরা গেলো..!! 😾🥀",
+				attachment: fs.createReadStream(__dirname + `/noprefix/attitude.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😹", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
