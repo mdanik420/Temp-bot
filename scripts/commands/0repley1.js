8a4ed5c28284e5b8af20fb2f3dc7500ee77c3289
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-17 21:57:24 +0800

    Update 0repley1.js

--- scripts/commands/0repley1.js
+++ scripts/commands/0repley1.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "reply1",
+  prefix: true,
+    version: "1.0.1",
+	permssion: 0,
+	credits: "Simamto", 
+	description: "hihihihi",
+	category: "no prefix",
+	usages: "reply1",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("assalamu alaikum")==0 || event.body.indexOf("assalamualaikum")==
+|| event.body.indexOf("asalamualaikum")==0 || event.body.indexOf("আসসালামুয়ালাইকুম")==0) {
+		var msg = {
+				body: "ওয়ালাইকুমুস সালাম 😇",
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }￼Not
 
