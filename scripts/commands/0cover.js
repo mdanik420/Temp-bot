d670a543f0424840056f7cedf4a794740b092d4c
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-18 18:29:52 +0800

    Update 0cover.js

--- scripts/commands/0cover.js
+++ scripts/commands/0cover.js
@@ -1,79 +1,201 @@
 module.exports.config = {
-  name: "cover",
-prefix: true,
-  version: "1.0.1",
-  permssion: 0,
-  credits: "CYBER-CAT",
-  description: "Create an interesting banner image",
-  category: "Create a photo",
-  usages: "cover [text1 - text2]",
-  cooldowns: 10,
-  dependencies: {
-    canvas: "",
-    axios: "",
+name: "fbcover",
+  prefix: true,
+version: "1.0.0",
+permssion: 0,
+credits: "Abdulla Rahaman",
+description: "use but can't change credit",
+category: "fbcover",
+usages: "facebook cover photo make by Abdulla Rahaman",
+cooldowns: 0,
+dependencies: {
     "fs-extra": "",
-  },
+    "request": "",
+    "axios": ""
+ }
 };
-
-module.exports.circle = async (image) => {
-  const jimp = global.nodemodule["jimp"];
-  image = await jimp.read(image);
-  image.circle();
-  return await image.getBufferAsync("image/png");
+module.exports.run = async function ({ api, args, event, permssion }) {
+if ((this.config.credits) != "Abdulla Rahaman") { return api.sendMessage(`[ WARNING ] - Abdulla Rahaman Don't credits modules ${this.config.name} was changed to ${this.config.credits} bởi ADMINBOT ${global.config.BOTNAME} 😐 Stop now!!!`, event.threadID, event.messageID)}
+    const request = require('request');
+  const fs = require("fs-extra")
+  const axios = require("axios")
+  const { threadID, messageID, senderID, body } = event;
+  if(!args[0]) return api.sendMessage(' আসসালামু আলাইকুম 🥀Hey Itz Me 𝗖𝗬𝗕𝗘𝗥-𝗖𝗔𝗧 অনুগ্রহ করে টাইপ করুন /fbcover তারপর প্রাথমিক নাম লিখুন!!!', threadID, messageID)
+  else return api.sendMessage(`😍 আপনি প্রধান নাম বেছে নিয়েছেন:${args.join(" ").toUpperCase()}\n\n(এই বার্তার উত্তর দিন এবং আপনার দ্বিতীয় নাম নির্বাচন করুন)`,event.threadID, (err, info) => {
+     return global.client.handleReply.push({
+        type: "tenphu",
+        name: `fbcover`,
+        author: senderID,
+        tenchinh: args.join(" ").toUpperCase(),
+        messageID: info.messageID
+      });
+  },event.messageID);
 }
-module.exports.run = async function ({ api, event, args, Users }) {
-  let { senderID, threadID, messageID } = event;
+module.exports.handleReply = async function({ api, event, args, handleReply, client, __GLOBAL, Threads, Users, Currencies }) {
+  module.exports.circle = async (image) => {
+    const jimp = require("jimp")
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+  }
+  if (handleReply.author != event.senderID) return;
+  const { threadID, messageID, senderID, body } = event;
   const { loadImage, createCanvas } = require("canvas");
   const request = require('request');
-  const fs = global.nodemodule["fs-extra"];
-  const axios = global.nodemodule["axios"];
-  let pathImg = __dirname + `/cache/${senderID}.png`;
-  let pathAva = __dirname + `/cache/avtuser.png`;
-  let text = args.join(" ")
-  if (!text) return api.sendMessage('💢Please enter the correct format [text1 - text2] ', event.threadID, event.messageID);
-  const text1 = text.substr(0, text.indexOf(' - ')); 
-  if (!text1) return api.sendMessage('💢Please enter the correct format [text1 - text2] ', event.threadID, event.messageID);
-  const text2 = text.split(" - ").pop()
-  if (!text2) return api.sendMessage('💢Please enter the correct format [text1 - text2] ', event.threadID, event.messageID);
-  let Avatar = (
-    await axios.get(
-      `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  let getWanted = (
-    await axios.get(encodeURI(`https://i.ibb.co/cCpB1sQ/Ph-i-b-a-trung-thu.png`), {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(pathAva, Buffer.from(Avatar, "utf-8"));
-  avatar = await this.circle(pathAva);
-  fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
-  let baseImage = await loadImage(pathImg);
-  let baseAva = await loadImage(avatar);
-  let canvas = createCanvas(baseImage.width, baseImage.height);
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(baseImage, 0, 0, 1920, 1080);
-  ctx.drawImage(baseAva, 820, 315, 283, 283);
-  ctx.font = "bold 70px Manrope";
-  ctx.fillStyle = "#ffff";
-  ctx.textAlign = "center";
-  fontSize = 40;
-  ctx.fillText(text1, 965, 715);
-  ctx.font = "55px Manrope";
-  ctx.fillStyle = "#ffff";
-  ctx.textAlign = "center";
-  fontSize = 20;
-  ctx.fillText(text2, 965, 800);
-  ctx.beginPath();
-  const imageBuffer = canvas.toBuffer();
-  fs.writeFileSync(pathImg, imageBuffer);
-  fs.removeSync(pathAva);
-  return api.sendMessage(
-    { attachment: fs.createReadStream(pathImg) },
-    threadID,
-    () => fs.unlinkSync(pathImg),
-    messageID
-  );
-};
-                   
+  const fs = require("fs-extra")
+  const axios = require("axios")
+  let pathImg = __dirname + `/cache/${senderID+20}.png`;
+  let pathAva = __dirname + `/cache/${senderID+30}.png`;
+  let pathLine = __dirname + `/cache/${senderID+40}.png`;
+  const path = require("path")
+  const Canvas = require("canvas")
+  const __root = path.resolve(__dirname, "cache");
+  var tenchinh = handleReply.tenchinh;
+  //=================CONFIG TEXT=============//
+  switch (handleReply.type) {
+    case "tenphu": {
+      var tenchinh = handleReply.tenchinh;
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`😍 আপনি দ্বিতীয় নাম বেছে নিয়েছেন${event.body.toUpperCase()}\n\n(
+এই বার্তার উত্তর দিন আপনার ফোন নম্বর দিন)`,threadID, function (err, info) {
+        return global.client.handleReply.push({
+          type: "sdt",
+          name: `fbcover`,
+          author: senderID,
+          tenphu: event.body,
+          tenchinh: tenchinh,
+          messageID: info.messageID
+        });
+      },messageID)
+    }
+    case "sdt": {
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`আপনি নাম্বার নির্বাচন করেছেন: ${event.body.toUpperCase()}\n\(আপনার ইমেল লিখে এই বার্তাটির উত্তর দিন)`,threadID, function (err, info) {
+        return global.client.handleReply.push({
+          type: "email",
+          name: `fbcover`,
+          author: senderID,
+          sdt: event.body,
+          tenchinh: handleReply.tenchinh,
+          tenphu: handleReply.tenphu,
+          messageID: info.messageID
+        });
+      },messageID) 
+    }
+    case "email": {
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`😘 আপনি আপনার ইমেল নির্বাচন করেছেন : ${event.body.toUpperCase()}\n\(আপনার ঠিকানা লিখে এই বার্তাটির উত্তর দিন)`,threadID, function (err, info) {
+        return global.client.handleReply.push({
+          type: "color",
+          name: `fbcover`,
+          author: senderID,
+          sdt: handleReply.sdt,
+          tenchinh: handleReply.tenchinh,
+          tenphu: handleReply.tenphu,
+          email: event.body,
+          messageID: info.messageID
+        });
+      },messageID) 
+    }
+    case "color": {
+      api.unsendMessage(handleReply.messageID);
+      return api.sendMessage(`আপনি ঠিকানা নির্বাচন করেছেন : ${event.body.toUpperCase()}\n আপনার কভার পিক এর ব্যাকগ্রাউন্ড কালার লিখে এই বার্তাটির উত্তর দিন(enter no as default color)`,threadID, function (err, info) {
+        return global.client.handleReply.push({
+          type: "create",
+          name: `fbcover`,
+          author: senderID,
+          sdt: handleReply.sdt,
+          tenchinh: handleReply.tenchinh,
+          tenphu: handleReply.tenphu,
+          diachi: event.body,
+          email: handleReply.email,
+          messageID: info.messageID
+        });
+      },messageID) 
+    }
+    case "create": {
+      var color = event.body
+      if (color.toLowerCase() == "no") var color = `#ffffff`
+      var address = handleReply.diachi.toUpperCase()
+      var name = handleReply.tenchinh.toUpperCase()
+      var email = handleReply.email.toUpperCase()
+      var subname = handleReply.tenphu.toUpperCase()
+      var phoneNumber = handleReply.sdt.toUpperCase()
+      api.unsendMessage(handleReply.messageID);
+      api.sendMessage(` আপনার কভার প্রায় হয়ে গেছে অনুগ্রহ করে অপেক্ষা করুন⏳ ইমেজার শুরু করা হচ্ছে!`,threadID, (err, info) => {
+      setTimeout(() => {
+              api.unsendMessage(info.messageID);
+     }, 1000);
+          }, messageID);
+      //=================CONFIG IMG=============//
+      let avtAnime = (
+        await axios.get(encodeURI(
+          `https://graph.facebook.com/${senderID}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`),
+          { responseType: "arraybuffer" }
+        )
+      ).data;
+      let background = (
+        await axios.get(encodeURI(`https://1.bp.blogspot.com/-ZyXHJE2S3ew/YSdA8Guah-I/AAAAAAAAwtQ/udZEj3sXhQkwh5Qn8jwfjRwesrGoY90cwCNcBGAsYHQ/s0/bg.jpg`), {
+          responseType: "arraybuffer",
+        })
+      ).data;
+      let hieuung = (
+        await axios.get(encodeURI(`https://1.bp.blogspot.com/-zl3qntcfDhY/YSdEQNehJJI/AAAAAAAAwtY/C17yMRMBjGstL_Cq6STfSYyBy-mwjkdQwCNcBGAsYHQ/s0/mask.png`), {
+          responseType: "arraybuffer",
+        })
+      ).data;
+      fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
+      fs.writeFileSync(pathImg, Buffer.from(background, "utf-8"));
+      fs.writeFileSync(pathLine, Buffer.from(hieuung, "utf-8"));
+      var avatar = await this.circle(pathAva);
+      //=================DOWNLOAD FONTS=============//
+      if(!fs.existsSync(__dirname+`/cache/UTMAvoBold.ttf`)) { 
+          let getfont2 = (await axios.get(`https://drive.google.com/u/0/uc?id=1DuI-ou9OGEkII7n8odx-A7NIcYz0Xk9o&export=download`, { responseType: "arraybuffer" })).data;
+           fs.writeFileSync(__dirname+`/cache/UTMAvoBold.ttf`, Buffer.from(getfont2, "utf-8"));
+        };
+      //=================DRAW BANNER=============//
+      let baseImage = await loadImage(pathImg);
+      let baseAva = await loadImage(avatar);
+      let baseLine = await loadImage(pathLine);
+      let canvas = createCanvas(baseImage.width, baseImage.height);
+      let ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+      Canvas.registerFont(__dirname+`/cache/UTMAvoBold.ttf`, { family: "UTMAvoBold" });
+      ctx.strokeStyle = "rgba(255,255,255, 0.2)";
+      ctx.lineWidth = 3;
+      ctx.font = "100px UTMAvoBold";
+      ctx.strokeText(name, 30, 100);
+      ctx.strokeText(name, 130, 200);
+      ctx.textAlign = "right";
+      ctx.strokeText(name, canvas.width - 30, canvas.height - 30);
+      ctx.strokeText(name, canvas.width - 130, canvas.height - 130);
+      ctx.fillStyle = `#ffffff`
+      ctx.font = "55px UTMAvoBold";
+      ctx.fillText(name, 680, 270);
+      ctx.font = "40px UTMAvoBold";
+      ctx.fillStyle = "#fff";
+      ctx.textAlign = "right";
+      ctx.fillText(subname, 680, 320);
+      ctx.font = "23px UTMAvoBold";
+      ctx.fillStyle = "#fff";
+      ctx.textAlign = "start";
+      ctx.fillText(phoneNumber, 1350, 252);
+      ctx.fillText(email, 1350, 332);
+      ctx.fillText(address, 1350, 410);
+      ctx.globalCompositeOperation = "destination-out";
+      ctx.drawImage(baseLine, 0, 0, canvas.width, canvas.height);
+      ctx.globalCompositeOperation = "destination-over";
+      ctx.fillStyle = color
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.globalCompositeOperation = "source-over";
+      ctx.drawImage(baseAva, 824, 180, 285, 285);
+      const imageBuffer = canvas.toBuffer();
+      fs.writeFileSync(pathImg, imageBuffer);
+      return api.sendMessage(
+        { attachment: fs.createReadStream(pathImg) },
+        threadID,messageID
+      );
+    }
+  }
+                                                                         }
