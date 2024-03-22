fd517ae19609f1e18675e5159b56ed414e524707
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:18:07 +0800

    Update AXOS18.js

--- scripts/commands/AXOS18.js
+++ scripts/commands/AXOS18.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("Amogus")==0 || event.body.indexOf("💃")==0 || 
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("Amogus")==0 || event.body.indexOf("💃")==0 || 
 event.body.indexOf("🤸")==0 ||      
 event.body.indexOf("🤸‍♂️")==0) {
 		var msg = {
