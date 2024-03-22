20d28eb1ff040e18dd5d83be2a54b3b8688396d6
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:28:22 +0800

    Update AXOS33.js

--- scripts/commands/AXOS33.js
+++ scripts/commands/AXOS33.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("Ami")==0 || event.body.indexOf("🙋")==0 || event.body.indexOf("🙋‍♂️")==0) {
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("Ami")==0 || event.body.indexOf("🙋")==0 || event.body.indexOf("🙋‍♂️")==0) {
 		var msg = {
 				body:"স্যার কেমন আছেন আপনি",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sir.mp3`)
