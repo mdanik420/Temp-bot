4f56b0cc943b7885fbfb957bc89ce93ed09bb609
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:28:48 +0800

    Update AXOS4.js

--- scripts/commands/AXOS4.js
+++ scripts/commands/AXOS4.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("🥱")==0 || event.body.indexOf("🥴")==0 || event.body.indexOf("😎")==0) {
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("🥱")==0 || event.body.indexOf("🥴")==0 || event.body.indexOf("😎")==0) {
 		var msg = {
 				body: "~  তুমি Attitude দেখাছো, তাতে আমার বাল ছেরা গেলো..!! 😾🥀",
 				attachment: fs.createReadStream(__dirname + `/noprefix/attitude.mp3`)
