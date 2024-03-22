72138fbf4f989aaf75dde6921aee3141b509fec9
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:22:49 +0800

    Update AXOS21.js

--- scripts/commands/AXOS21.js
+++ scripts/commands/AXOS21.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("18+")==0 || event.body.indexOf("Video de")==0 || event.body.indexOf("🍆")==0) {
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("18+")==0 || event.body.indexOf("Video de")==0 || event.body.indexOf("🍆")==0) {
 		var msg = {
 				body: "~ এই তুই ভালো হবি কবে 😾🔨..!",
 				attachment: fs.createReadStream(__dirname + `/noprefix/hobikobe.mp3`)
