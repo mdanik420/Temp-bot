5e7dab2737c9ceb7aca3c249f4ef8dc42f60042e
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:27:13 +0800

    Update AXOS31.js

--- scripts/commands/AXOS31.js
+++ scripts/commands/AXOS31.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("💃")==0 || event.body.indexOf("🤸‍♀️")==0 || event.body.indexOf("🤸‍♂️")==0) {
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("💃")==0 || event.body.indexOf("🤸‍♀️")==0 || event.body.indexOf("🤸‍♂️")==0) {
 		var msg = {
 				body: " ~ ওই সাবধানে উলটে পরে যাবা তো..!!",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sabdan.mp3`)
