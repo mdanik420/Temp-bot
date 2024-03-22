ded2877c6589b24900c56c84311f2da56e732529
mdanik420 <141121443+mdanik420@users.noreply.github.com>
2024-03-22 23:19:12 +0800

    Update AXOS20.js

--- scripts/commands/AXOS20.js
+++ scripts/commands/AXOS20.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("amogus")==0 || event.body.indexOf("শুভো রাত্রি")==0 || event.body.indexOf("Good night")==0 || event.body.indexOf("gd n8")==0) {
+	if (event.body.indexOf("voiceckanik")==0 || event.body.indexOf("শুভো রাত্রি")==0 || event.body.indexOf("Good night")==0 || event.body.indexOf("gd n8")==0) {
 		var msg = {
 				body: "~ ঘুমাবা নাকি জান, আচ্ছা ঘুমিয়ে পরো, সকাল এ কথা হবে, শুভ রাত্রি..!!",
 				attachment: fs.createReadStream(__dirname + `/noprefix/goodnight.mp3`)
